7288a068d0a8b86c40ce8ba697c5127cdec6dbbc
maany <imptodefeat@gmail.com>
2019-10-21 18:11:16 +0200

    fix ALLOW_WRITE

--- sh/pre_config/files/pc_config_50.py
+++ sh/pre_config/files/pc_config_50.py
@@ -9,7 +9,7 @@ class PCConfig50(ConfigFile):
         super().add_advanced_parameters()
         batch_dns = self.get_batch_dns_info()
         batch_ip = batch_dns['container_ip']
-        batch_subnet = '.'.join((batch_ip.split('.')[0:-2] + ['0', '0']))
-        self.advanced_category.add("Use ROLE: Submit\n")
+        allow_write = '.'.join((batch_ip.split('.')[0:-2] + ['*', '*']))
+        self.advanced_category.add("Use ROLE: submit\n")
         self.advanced_category.add_key_value("CONDOR_HOST", batch_ip)
-        self.advanced_category.add_key_value("ALLOW_WRITE", batch_subnet)
+        self.advanced_category.add_key_value("ALLOW_WRITE", allow_write)
