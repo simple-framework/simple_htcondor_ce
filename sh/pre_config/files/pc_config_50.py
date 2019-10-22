751585bc9b2d12e29063053f804f761dcf30688b
maany <imptodefeat@gmail.com>
2019-10-22 18:17:27 +0200

    fix schedd allow_write

--- sh/pre_config/files/pc_config_50.py
+++ sh/pre_config/files/pc_config_50.py
@@ -10,7 +10,7 @@ class PCConfig50(ConfigFile):
         super().add_advanced_parameters()
         batch_dns = get_batch_dns_info(self.augmented_site_level_config)
         batch_ip = batch_dns['container_ip']
-        allow_write = '.'.join((batch_ip.split('.')[0:-2] + ['*', '*']))
+        allow_write = '.'.join((batch_ip.split('.')[0:-2] + ['*']))
         self.advanced_category.add("Use ROLE: submit\n")
         self.advanced_category.add_key_value("CONDOR_HOST", batch_ip)
         self.advanced_category.add_key_value("ALLOW_WRITE", allow_write)
